2ec36446f6da606945fc78e7b59df2e2b9be713c
Dušan Devedžić <80401917+Ddevedza@users.noreply.github.com>
2026-03-21 22:41:34 +0100

    Update proc_load_gold.sql

--- scripts/gold/proc_load_gold.sql
+++ scripts/gold/proc_load_gold.sql
@@ -95,7 +95,47 @@ SELECT
 	utilization -- total encounter count at this organization
 FROM silver.organizations
 
+GO
 
+TRUNCATE TABLE gold.dim_payer;
+
+INSERT INTO gold.dim_payer (
+    payer_id,
+    payer_name,
+    payer_address,
+    payer_city,
+    payer_state,
+    payer_zip,
+    payer_phone,
+    amount_covered,
+    amount_uncovered,
+    revenue,
+    covered_encounters,
+    uncovered_encounters,
+    covered_medications,
+    uncovered_medications,
+    covered_procedures,
+    uncovered_procedures,
+    covered_immunizations,
+    uncovered_immunizations,
+    unique_customers,
+    qols_avg,
+    member_months
+)
+SELECT
+    id AS payer_id,
+    name AS payer_name,
+    ISNULL(address, 'unknown') AS payer_address,  -- NULL handling for missing address data
+    ISNULL(city, 'unknown') AS payer_city,
+    ISNULL(state_headquartered, 'unknown') AS payer_state,
+    ISNULL(zip, 'unknown') AS payer_zip,
+    ISNULL(phone, 'unknown') AS payer_phone,
+    amount_covered,
+    amount_uncovered,
+    revenue,
+    qols_avg,   -- capped to [0,1] in Silver layer
+    member_months
+FROM silver.payers;
 
 GO
 

e261f8b8a9711d67bd48f02a18b2048c5afa3586
Dušan Devedžić <80401917+Ddevedza@users.noreply.github.com>
2026-03-27 00:22:02 +0100

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -89,6 +89,7 @@ CREATE TABLE gold.dim_payer(
     amount_covered          DECIMAL(18,2),       -- total amount covered by payer
     amount_uncovered        DECIMAL(18,2),       -- total amount not covered
     revenue                 DECIMAL(18,2),
+    qols_avg                FLOAT,              -- quality of life score [0-1], capped in Silver
     member_months           INT,                 -- total member months of coverage
     dwh_create_date         DATETIME2 DEFAULT GETDATE()
 );

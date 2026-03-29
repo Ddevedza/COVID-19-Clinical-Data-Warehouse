5845fdf5b9da86201db8ffbf934a83ce0cbd6431
Dušan Devedžić <80401917+Ddevedza@users.noreply.github.com>
2026-03-29 21:58:24 +0200

    Refactor vw_condition_prevalence view and clean up SQL
    Updated SQL view for condition prevalence with comments and removed unnecessary queries.

--- scripts/views/vw_condition_prevalence.sql
+++ scripts/views/vw_condition_prevalence.sql
@@ -9,18 +9,10 @@
 CREATE VIEW gold.vw_condition_prevalence AS
 SELECT
 	code,
-	description AS condition_name,
-	COUNT(DISTINCT patient_key) AS patient_count,
-	COUNT(*) AS total_diagnoses,
-	SUM(CASE WHEN is_active = 1 THEN 1 ELSE 0 END) AS active_cases,
-    SUM(CASE WHEN is_active = 0 THEN 1 ELSE 0 END) AS resolved_cases
+	description AS condition_name, 
+	COUNT(DISTINCT patient_key) AS patient_count, -- total patients
+	COUNT(*) AS total_diagnoses, -- total diagnoses
+	SUM(CASE WHEN is_active = 1 THEN 1 ELSE 0 END) AS active_cases, -- total active cases
+    SUM(CASE WHEN is_active = 0 THEN 1 ELSE 0 END) AS resolved_cases -- total resolved cases
 FROM gold.fact_condition
 GROUP BY code,description;
-
-
-SELECT TOP 10 * FROM gold.vw_condition_prevalence
-ORDER BY patient_count DESC;
-
--- Find all diabetic patients
-SELECT * FROM gold.vw_condition_prevalence
-WHERE condition_name LIKE '%diabetes%';

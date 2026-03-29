7612222f741a8570ee07bce5ce0c9159924d53ff
Dušan Devedžić <80401917+Ddevedza@users.noreply.github.com>
2026-03-29 21:21:13 +0200

    Create gold.vw_condition_prevalence.sql

---
+++ scripts/views/gold.vw_condition_prevalence.sql
@@ -0,0 +1,10 @@
+CREATE VIEW gold.vw_condition_prevalence AS
+SELECT
+	code,
+	description AS condition_name,
+	COUNT(DISTINCT patient_key) AS patient_count,
+	COUNT(*) AS total_diagnoses,
+	SUM(CASE WHEN is_active = 1 THEN 1 ELSE 0 END) AS active_cases,
+    SUM(CASE WHEN is_active = 0 THEN 1 ELSE 0 END) AS resolved_cases
+FROM gold.fact_condition
+GROUP BY code,description;
